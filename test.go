bd7da616e5a12a9bed50e3b8d27cb88c55361ca7
Seo <dk23049@dankook.sen.hs.kr>
2023-09-12 18:29:25 +0900

    Update test.go

--- test.go
+++ test.go
@@ -8,6 +8,21 @@ func split(sum int) (엑스, 와이 int) {
 	return
 }
 
+func swap(x, y string) (string, string) {
+	return x + y, y + x
+}
+
+func pow(x, n, lim float64) float64 {
+	if v < lim {
+    	return v
+    }
+    return lim
+}
+
 func main() {
+	c := "no"
 	fmt.Println(split(17))
+	fmt.Println(swap("하세요", "안녕"))
+	fmt.Printf("The Type of c is %T\n", c)
+	fmt.Printf(pow(5, 2, 25.1))
 }
